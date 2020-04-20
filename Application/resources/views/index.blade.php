48507b7535c05593ed4a5e8ec5e8bfbee008a95e
maaaaakoto35 <hajimeyo13@icloud.com>
2020-04-20 21:49:01 +0900

    [develop] fix route for js

--- Application/resources/views/index.blade.php
+++ Application/resources/views/index.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Student Cafeteria</title>
-    <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/jquery.bxslider.css">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/jquery.bxslider.css') }}">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script type="text/javascript" src="js/jquery.bxSlider.js"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery.bxSlider.js') }}"></script>
 
     <script type="text/javascript">
             $(document).ready(function(){
