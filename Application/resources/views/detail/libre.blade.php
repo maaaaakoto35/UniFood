0ff2615d5e812f0b5d2956b883a79560892cc7f5
maaaaakoto35 <hajimeyo13@icloud.com>
2020-05-02 00:32:49 +0900

    [fujiwara] fix details for rate

--- Application/resources/views/detail/libre.blade.php
+++ Application/resources/views/detail/libre.blade.php
@@ -18,10 +18,10 @@
 
             <div class="rate">
                 @if ($star)
-                    <div class="star-rating-front" style="style: inline-block; width: {{$star}}%">★★★★★</div>
-                    <div class="star-rating-back" style="style: inline-block;">★★★★★</div> : {{$result["rate"]}}
+                    <div class="star-rating-front" style="display: inline-block; width: {{$star}}%">★★★★★</div>
+                    <div class="star-rating-back" style="display: inline-block;">★★★★★</div> : {{$result["rate"]}}
                 @else
-                    <div class="star-rating-back style="style: inline-block;"">★★★★★</div> : まだ口コミがありません
+                    <div class="star-rating-back display: inline-block;">★★★★★</div> : まだ口コミがありません
                 @endif
             </div>
         </div>
