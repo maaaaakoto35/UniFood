828557dfc6fb2eb55a9197139f046942ce1ef45b
maaaaakoto35 <hajimeyo13@icloud.com>
2020-05-03 18:33:08 +0900

    [fujiwara] add auth secound

--- Application/routes/web.php
+++ Application/routes/web.php
@@ -34,3 +34,11 @@ Route::view('/log_in', 'log_in');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
