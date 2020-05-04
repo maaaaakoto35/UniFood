0c530798dc470695e9660696e0c78eb7eeee1a8e
maaaaakoto35 <hajimeyo13@icloud.com>
2020-05-04 22:39:52 +0900

    [privious] add table and login and signup for view

---
+++ Application/resources/views/login.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ログイン</title>
+    <link rel="stylesheet" href="{{ asset('css/other.css') }}">
+</head>
+<body>
+    <div class="wrapper">
+        @include('header')
+
+        <div class="form">
+            <form action="login" method="POST">
+                <input type="text" size="50" name="e-mail" placeholder="メールアドレスを入力して下さい">
+                <input type="password" size="30" name="password" placeholder="パスワード">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
