b51c2e95684f4a99bd5b647a735fa421602655ad
maaaaakoto35 <hajimeyo13@icloud.com>
2020-05-07 22:38:58 +0900

    [fujiwara] add signup.css

--- Application/resources/views/signup.blade.php
+++ Application/resources/views/signup.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>会員登録</title>
-    <link rel="stylesheet" href="{{ asset('css/other.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/signup.css') }}">
 </head>
 <body>
     <div class="wrapper">
@@ -18,12 +18,32 @@
         <div class="form">
             <form action="/signup" method="POST" enctype="multipart/form-data">
                 @csrf
-                <input type="text" size="50" name="name" placeholder="名前">
+                <div class="form">
+                    <input class="ef" size="50" type="text" placeholder="名前" name="name">
+                    <label></label>
+                    <span class="focus_line"></span>
+                </div><br><br>
+                <div class="form">
+                    <input class="ef" size="50" type="text" placeholder="メールアドレス" name="e-mail">
+                    <label></label>
+                    <span class="focus_line"></span>
+                </div><br><br>
+                <div class="form">
+                    <input class="ef" size="6" type="text" placeholder="学籍番号(半角英数)" name="student_number">
+                    <label></label>
+                    <span class="focus_line"></span>
+                </div><br><br>
+                <div class="form">
+                    <input class="ef" size="30" type="password" placeholder="パスワード" name="password">
+                    <label></label>
+                    <span class="focus_line"></span>
+                </div><br><br>
+                {{-- <input type="text" size="50" name="name" placeholder="名前">
                 <input type="text" size="50" name="e-mail" placeholder="メールアドレス">
-                <input type="text" size="50" name="student_number" placeholder="学籍番号(半角英数)">
-                <input type="password" size="30" name="password" placeholder="パスワード">
+                <input type="text" size="6" name="student_number" placeholder="学籍番号(半角英数)">
+                <input type="password" size="30" name="password" placeholder="パスワード"> --}}
                 <input type="file" name="image" class="form-control">
-                <input type="submit" value="登録">
+                <input type="submit" class="submit_button" value="登録">
             </form>
         </div>
     </div>
