3886e4582939e6f87947c285f9897ef3145e42d9
maaaaakoto35 <hajimeyo13@icloud.com>
2020-05-07 21:35:44 +0900

    [fujiwara] add login.css

--- Application/resources/views/login.blade.php
+++ Application/resources/views/login.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ログイン</title>
-    <link rel="stylesheet" href="{{ asset('css/other.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body>
     <div class="wrapper">
@@ -19,13 +19,25 @@
             @endif
         </div>
 
-        <div class="form">
-            <form action="/login" method="POST">
-                @csrf
-                <input type="text" size="50" name="e-mail" placeholder="メールアドレスを入力して下さい">
-                <input type="password" size="30" name="password" placeholder="パスワード">
-                <input type="submit" value="ログイン">
-            </form>
+        <div class="main">
+            <div class="content">
+                <form action="/login" method="POST">
+                    @csrf
+                    <div class="form">
+                        <input class="ef" size="50" type="text" placeholder="メールアドレスを入力して下さい" name="e-mail">
+                        <label></label>
+                        <span class="focus_line"></span>
+                    </div>
+                    <div class="form">
+                        <input class="ef" size="30" type="password" placeholder="パスワード" name="password">
+                        <label></label>
+                        <span class="focus_line"></span>
+                    </div>
+                    {{-- <input type="text" size="50" name="e-mail" placeholder="メールアドレスを入力して下さい">
+                    <input type="password" size="30" name="password" placeholder="パスワード"> --}}
+                    <input type="submit" value="ログイン">
+                </form>
+            </div>
         </div>
 
         <div class="sign_up_link">
