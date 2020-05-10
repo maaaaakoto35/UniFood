7cb1c064aa54b44d517c69d434aa72cd21c829a9
yusuke-777 <yyy120326@gmail.com>
2020-05-10 14:37:31 +0900

    [yusuke] fix conflict

--- Application/resources/views/detail/hannnari.blade.php
+++ Application/resources/views/detail/hannnari.blade.php
@@ -28,56 +28,59 @@
                 </div>
             @endif
         </div>
-        <main>
-            <ul class="tabs">
-                <li><a href="" class="active">はんなり食堂</a></li>
-                <li><a href="">メニュー</a></li>
-                <li><a href="">口コミ</a></li>
+       
+       <div class="tab-panel">
+            <ul class="tabs-group">
+                <li class="tab tab-A is-active">はんなり食堂</li>
+                <li class="tab tab-B">メニュー</li>
+                <li class="tab tab-C">口コミ</li>
             </ul>
 
-            <ul class="contents">
-                <li class="active">
-                    <img src="{{ asset('img/'.$result["store_name"].'/Shokudo.jpg') }}" width="280" height="210" alt="logo">
-                    {{-- <img src="img/foods/food2.jpg" width="240" height="180" alt="">
-                    <img src="img/foods/food3.jpg" width="240" height="180" alt="">
-                    <img src="img/foods/food4.jpg" width="240" height="180" alt="">
-                    <img src="img/foods/food5.jpg" width="240" height="180" alt="">
-                    <img src="img/foods/food6.jpg" width="240" height="180" alt="">
-                    <img src="img/foods/food7.jpg" width="240" height="180" alt="">
-                    <img src="img/foods/food8.jpg" width="240" height="180" alt="">
-                    <img src="img/foods/food9.jpg" width="240" height="180" alt=""> --}}
-                    <table>
-                        <tr>ジャンル: {{$result["genre"]}} </tr>
-                        <tr>価格帯: {{$result["price"]}} </tr>
-                        <tr>営業時間: {{$result["open_time"]}} </tr>
-                    </table><br><br>
-                    <section>
-                        低価格でボリュームある定食が人気の食堂。<br>
-                        日替りランチのバリエーションも<br>
-                        豊富で、カレー、丼物、麺類の品揃えも充実。<br>
-                        味と量に大満足です。うどん安すぎ<br>
-                    </section>
-                </li>
+            <div class="panel-group">
+                <div class="panel tab-A is-show">
+                    <ul>
+                        <img src="{{ asset('img/'.$result["store_name"].'/Shokudo.jpg') }}" width="280" height="210" alt="logo">
+                        {{-- <img src="img/foods/food2.jpg" width="240" height="180" alt="">
+                        <img src="img/foods/food3.jpg" width="240" height="180" alt="">
+                        <img src="img/foods/food4.jpg" width="240" height="180" alt="">
+                        <img src="img/foods/food5.jpg" width="240" height="180" alt="">
+                        <img src="img/foods/food6.jpg" width="240" height="180" alt="">
+                        <img src="img/foods/food7.jpg" width="240" height="180" alt="">
+                        <img src="img/foods/food8.jpg" width="240" height="180" alt="">
+                        <img src="img/foods/food9.jpg" width="240" height="180" alt=""> --}}
+                        <table>
+                            <tr>ジャンル: {{$result["genre"]}} </tr>
+                            <tr>価格帯: {{$result["price"]}} </tr>
+                            <tr>営業時間: {{$result["open_time"]}} </tr>
+                        </table><br><br>
+                        <li>
+                            低価格でボリュームある定食が人気の食堂。<br>
+                            日替りランチのバリエーションも<br>
+                            豊富で、カレー、丼物、麺類の品揃えも充実。<br>
+                            味と量に大満足です。うどん安すぎ<br>
+                        </li>
+                    </ul>
+                </div>
 
-                <li>
+                <div class="panel tab-B">
                     @foreach ($menus as &$menu)
-                         <span>{{$menu["food_name"]}}</span>
-                            <span>{{$menu["price"]}}円</span>
+                        <span>{{$menu["food_name"]}}</span>
+                        <span>{{$menu["price"]}}円</span>
                     @endforeach
-                </li>
-    
-                 <li>
+                </div>
+
+                <div class="panel tab-C">
                     <h2>{{$result["store_jname"]}}の口コミ</h2>
                     @foreach ($posts as $post)
-                            <p>{{$post["title"]}}</p>
-                            <p>{{$post["contents"]}}</p>
+                        <p>{{$post["title"]}}</p>
+                        <p>{{$post["contents"]}}</p>
                         @if (isset($post["img_name"]))
                             <img src="{{ asset($post["img_path"].$post["img_name"]) }}" alt="口コミの画像">
                         @endif
                     @endforeach
-                </li>
-            </ul>
-        </main>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
